2281ee928d0dd1bd45537b6965c4027f70946076
Juraj Kirchheim <back2dos@googlemail.com>
2020-08-13 00:33:22 +0200

    Adjust to Haxe 4.2.

--- src/tink/domspec/ClassName.hx
+++ src/tink/domspec/ClassName.hx
@@ -2,23 +2,24 @@ package tink.domspec;
 
 using StringTools;
 
+@:transitive
 abstract ClassName(String) to String {
-  
+
   inline function new(s:String) this = s;
-  
-  public function add(that:ClassName) 
+
+  public function add(that:ClassName)
     return new ClassName(switch [this, (that:String)] {
       case [null, v] | [v, null]: v;
       case [a, b]: '$a $b';
     });
-    
+
   public function when(cond:Bool):ClassName
     return new ClassName(if(cond) this else '');
 
   @:from static function ofMap(parts:Map<String, Bool>)
     return new ClassName(ofArray([for (c in parts.keys()) if (parts[c]) ofString(c)]));
-  
-  @:from static function ofArray(parts:Array<String>) 
+
+  @:from static function ofArray(parts:Array<String>)
     return new ClassName(parts.map(ofString).join(' '));
 
   @:from static function ofString(s:String):ClassName
