bea254e51022ead3ebd214a981b621749c3cbc89
Juraj Kirchheim <back2dos@gmail.com>
2021-12-18 18:35:05 +0100

    Add helper to augment taglike functions with class names.

--- src/tink/domspec/ClassName.hx
+++ src/tink/domspec/ClassName.hx
@@ -1,5 +1,11 @@
 package tink.domspec;
 
+#if macro
+import haxe.macro.Type;
+import haxe.macro.*;
+using haxe.macro.Tools;
+using tink.MacroApi;
+#end
 using StringTools;
 
 @:transitive
@@ -22,11 +28,85 @@ abstract ClassName(String) to String {
   @:from static function ofArray(parts:Array<String>)
     return new ClassName(parts.map(ofString).join(' '));
 
-  @:from static function ofString(s:String):ClassName
-    return if (s == null) null else new ClassName(s.trim());
-
   @:from static function ofDynamicAccess(parts:haxe.DynamicAccess<Bool>)
     return new ClassName(ofArray([for (c in parts.keys()) if (parts[c]) ofString(c)]));
+
+  #if macro @:from #end static function ofString(s:String):ClassName
+    return if (s == null) null else new ClassName(s.trim());
+
+  public function applyVia<V, O:{ @:optional var className(default, never):V; }>(o:O, via:ClassName->V->V) {
+    var ret:O =
+      #if js
+        js.lib.Object.create(o)
+      #else
+        Reflect.copy(o)
+      #end
+    ;
+
+    (cast ret).className = via(cast this, o.className);
+    return ret;
+  }
+
+  macro public function applyTo(ethis, e:Expr) {
+
+    function generate(e, args, ret) {
+      if (false) TFun(args, ret);
+      var callArgs = [];
+      var f:Expr.Function = {
+        args: [],
+        ret: null,
+        expr: macro return $e($a{callArgs}),
+      }
+      var found = false;
+
+      for(a in args) {
+        f.args.push({
+          type: null,
+          name: a.name,
+          opt: a.opt
+        });
+
+        var arg = macro $i{a.name};
+        callArgs.push(switch a.t.reduce() {
+          case TAnonymous([for (f in _.get().fields) if (f.name == 'className') f] => [f]):
+
+            found = true;
+
+            var via = switch f.type.reduce() {
+              case TAbstract(_.get() => { module: 'tink.hxx.Expression' }, _):
+                macro @:pos(e.pos) (c, a) -> () -> c.add(a());
+              default:
+                macro @:pos(e.pos) (c, a) -> c.add(a);
+            }
+
+            macro @:pos(e.pos) _tink_tmp.applyVia($arg, $via);
+          default: macro $arg;
+        });
+      }
+
+      if (!found)
+        e.reject('no argument found that has a className property');
+
+      return macro {
+        var _tink_tmp = $ethis;
+        ${f.asExpr()};
+      }
+    }
+
+    return switch Context.typeof(e) {
+      case _.reduce() => TFun(args, ret):
+        generate(e, args, ret);
+      case TAnonymous(_.get() => { fields: [for (f in _) if (f.name == 'fromHxx') f] => [f] }):
+        switch f.type.reduce() {
+          case TFun(args, ret):
+            generate(macro @:pos(e.pos) $e.fromHxx, args, ret);
+          default:
+            f.pos.error('foo');
+        };
+      case t:
+        e.reject('${t.toString()} is not a function');
+    }
+  }
 
   @:from macro static function ofExpr(e:Expr)
     return switch e {
